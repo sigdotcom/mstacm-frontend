a7732da0ea6d826c7ba57adc9f199ff283bba2ef
Carter <cjwagn1@gmail.com>
2023-10-09 03:15:30 -0500

    Update config and api

--- src/pages/Dashboard/tools/Profile.tsx
+++ src/pages/Dashboard/tools/Profile.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { useListUsers } from "../../../api/users";
 
 const Profile = () => {
+  const { data, error, isLoading } = useListUsers();
+
+  console.log(data);
+  console.error(error);
   return (
     <div>
       <p>Profile</p>
+      {isLoading && <p>Loading...</p>}
     </div>
   );
 };
