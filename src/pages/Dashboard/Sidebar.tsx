0bc3ef516b7d8368a17b206b0e0dcfbda71f49ac
Carter Wagner <37089795+cjwagn1@users.noreply.github.com>
2023-10-10 02:57:44 -0500

    Added permission tool (#12)

--- src/pages/Dashboard/Sidebar.tsx
+++ src/pages/Dashboard/Sidebar.tsx
@@ -21,6 +21,72 @@ const Sidebar: React.FC<SidebarProps> = ({ tools, isDesktop }) => {
   const [open, setOpen] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
 
+  const sidebarContent = (
+    <Box
+      onMouseEnter={() => isDesktop && setIsHovered(true)}
+      onMouseLeave={() => isDesktop && setIsHovered(false)}
+      sx={{
+        width: isDesktop ? (isHovered ? "400px" : "60px") : "100%",
+        height: "100%",
+        position: "fixed",
+        zIndex: 1000,
+        backgroundColor: "#0392ce",
+        color: "white",
+      }}
+    >
+      {!isDesktop && (
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            gap: 0.5,
+            ml: "auto",
+            mt: 1,
+            mr: 1,
+          }}
+        >
+          <ModalClose
+            id="close-icon"
+            sx={{ position: "initial" }}
+            onClick={() => setOpen(false)}
+          />
+        </Box>
+      )}
+      <List
+        size="lg"
+        component="nav"
+        sx={{
+          flex: "none",
+          mt: 6,
+          mb: 5,
+          fontSize: "xl",
+          "& > a": {
+            display: "flex",
+            textDecoration: "none",
+            width: "100%",
+          },
+        }}
+      >
+        {tools.map((tool) => (
+          <Link to={"/dashboard/" + tool.path}>
+            <ListItemButton
+              key={tool.name}
+              onClick={() => setOpen(false)}
+              sx={{
+                fontWeight: "lg",
+                justifyContent: "center",
+                width: "100%",
+                color: "white",
+              }}
+            >
+              {isHovered || !isDesktop ? tool.name : tool.icon}
+            </ListItemButton>
+          </Link>
+        ))}
+      </List>
+    </Box>
+  );
+
   return (
     <React.Fragment>
       {/* Button to open drawer on mobile */}
@@ -34,75 +100,24 @@ const Sidebar: React.FC<SidebarProps> = ({ tools, isDesktop }) => {
           <Menu />
         </IconButton>
       )}
-      <Drawer
-        open={isDesktop || open}
-        onClose={() => setOpen(false)}
-        hideBackdrop={true}
-        size="sm"
-        sx={{
-          "--Drawer-horizontalSize": isDesktop
-            ? isHovered
-              ? "400px"
-              : "60px"
-            : "100%",
-        }}
-      >
-        <Box
-          onMouseEnter={() => isDesktop && setIsHovered(true)}
-          onMouseLeave={() => isDesktop && setIsHovered(false)}
-          sx={{ width: "100%", height: "100%" }}
+      {isDesktop ? (
+        sidebarContent
+      ) : (
+        <Drawer
+          open={open}
+          onClose={() => setOpen(false)}
+          hideBackdrop={true}
+          size="sm"
+          sx={{
+            "--Drawer-horizontalSize": "100%",
+            position: "relative",
+            zIndex: 1000,
+            width: "100%",
+          }}
         >
-          {!isDesktop && (
-            <Box
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                gap: 0.5,
-                ml: "auto",
-                mt: 1,
-                mr: 1,
-              }}
-            >
-              <ModalClose
-                id="close-icon"
-                sx={{ position: "initial" }}
-                onClick={() => setOpen(false)}
-              />
-            </Box>
-          )}
-          <List
-            size="lg"
-            component="nav"
-            sx={{
-              flex: "none",
-              mt: 6,
-              mb: 5,
-              fontSize: "xl",
-              "& > a": {
-                display: "flex",
-                textDecoration: "none",
-                width: "100%",
-              },
-            }}
-          >
-            {tools.map((tool, index) => (
-              <Link to={"/dashboard/" + tool.path}>
-                <ListItemButton
-                  key={tool.name}
-                  onClick={() => setOpen(false)}
-                  sx={{
-                    fontWeight: "lg",
-                    justifyContent: "center",
-                    width: "100%",
-                  }}
-                >
-                  {isHovered || !isDesktop ? tool.name : tool.icon}{" "}
-                </ListItemButton>
-              </Link>
-            ))}
-          </List>
-        </Box>
-      </Drawer>
+          {sidebarContent}
+        </Drawer>
+      )}
     </React.Fragment>
   );
 };
